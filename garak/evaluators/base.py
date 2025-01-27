6f429cf91ec796f536c9b78f699228a2bd86f473
Leon Derczynski <leonderczynski@gmail.com>
2025-01-27 11:05:35 +0100

    serialise turns in eval logging

--- garak/evaluators/base.py
+++ garak/evaluators/base.py
@@ -109,8 +109,8 @@ class Evaluator:
                             json.dumps(
                                 {
                                     "goal": attempt.goal,
-                                    "prompt": attempt.prompt.to_dict,
-                                    "output": [o.to_dict for o in attempt.all_outputs[idx]],
+                                    "prompt": attempt.prompt.to_dict(),
+                                    "output": attempt.all_outputs[idx].to_dict(),
                                     "trigger": trigger,
                                     "score": score,
                                     "run_id": str(_config.transient.run_id),
