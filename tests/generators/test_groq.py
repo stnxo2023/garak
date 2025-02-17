af4b40973a2aaf61682e34299b9bb82ccf6887ce
Leon Derczynski <lderczynski@nvidia.com>
2025-02-17 14:37:26 +0100

    migrate groq to Turn

--- tests/generators/test_groq.py
+++ tests/generators/test_groq.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 
+from garak.attempt import Turn
 from garak.generators.groq import GroqChat
 
 
@@ -10,7 +11,7 @@ def test_groq_invalid_multiple_completions():
     with pytest.raises(AssertionError) as e_info:
         generator = GroqChat(name="llama3-8b-8192")
         generator._call_model(
-            prompt="this is expected to fail", generations_this_call=2
+            prompt=Turn("this is expected to fail"), generations_this_call=2
         )
     assert "n > 1 is not supported" in str(e_info.value)
 
@@ -25,17 +26,17 @@ def test_groq_instantiate():
 
 @pytest.mark.skipif(
     os.getenv(GroqChat.ENV_VAR, None) is None,
-    reason=f"GroqChat  API key is not set in {GroqChat.ENV_VAR}",
+    reason=f"GroqChat API key is not set in {GroqChat.ENV_VAR}",
 )
 def test_groq_generate_1():
     g = GroqChat(name="llama3-8b-8192")
-    result = g._call_model("this is a test", generations_this_call=1)
+    result = g._call_model(Turn("this is a test"), generations_this_call=1)
     assert isinstance(result, list), "GroqChat _call_model should return a list"
     assert len(result) == 1, "GroqChat _call_model result list should have one item"
-    assert isinstance(result[0], str), "GroqChat generate() should contain a str"
-    result = g.generate("this is a test", generations_this_call=1)
+    assert isinstance(result[0], Turn), "GroqChat generate() should contain a str"
+    result = g.generate(Turn("this is a test"), generations_this_call=1)
     assert isinstance(result, list), "GroqChat generate() should return a list"
     assert (
         len(result) == 1
     ), "GroqChat generate() result list should have one item when generations_this_call=1"
-    assert isinstance(result[0], str), "GroqChat generate() should contain a str"
+    assert isinstance(result[0], Turn), "GroqChat generate() should contain a str"
