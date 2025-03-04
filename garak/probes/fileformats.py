9561700957bc3a02a0ab80c5748b0681a94e0297
Leon Derczynski <leonderczynski@gmail.com>
2025-03-04 16:34:36 +0100

    rm generations set in fileformats probe

--- garak/probes/fileformats.py
+++ garak/probes/fileformats.py
@@ -46,7 +46,6 @@ class HF_Files(Probe, Configurable):
 
     def __init__(self, config_root=_config):
         self._load_config(config_root)
-        self.generations = 1  # force generations to 1, probe preforms a static test
         super().__init__(config_root=config_root)
 
     def probe(self, generator) -> Iterable[garak.attempt.Attempt]:
