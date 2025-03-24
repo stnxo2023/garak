ce9fc38b4c5138559f999acda652bd5a42d46311
Erwan ROUSSEL <e-roussel@outlook.com>
2025-03-24 15:06:07 +0100

    Define ENV_VAR and DEFAULT_PARAMS before __init__
    Co-authored-by: Jeffrey Martin <jmartin@Op3n4M3.dev>
    Signed-off-by: Erwan ROUSSEL <e-roussel@outlook.com>

--- garak/generators/mistral.py
+++ garak/generators/mistral.py
@@ -9,15 +9,31 @@ class MistralGenerator(Generator):
     generator_family_name = "mistral"
     fullname = "Mistral AI"
     supports_multiple_generations = False
+    ENV_VAR="MISTRAL_API_KEY"`
+    DEFAULT_PARAMS = Generator.DEFAULT_PARAMS | { 
+        "name": "mistral-large-latest",
+    }
 
-    def __init__(self, name="mistral-large-latest", config_root=_config):
-        self.name = name
-        api_key = os.getenv("MISTRAL_API_KEY")
-        if not api_key:
-            raise ValueError(
-                "Please define mistral api key by defining environment variable MISTRAL_API_KEY"
-            )
-        self.client = Mistral(api_key=api_key)
+    # avoid attempt to pickle the client attribute
+    def __getstate__(self) -> object:
+        self._clear_client()
+        return dict(self.__dict__)
+
+    # restore the client attribute
+    def __setstate__(self, d) -> object:
+        self.__dict__.update(d)
+        self._load_client()
+
+    def _load_client(self):
+        self.client = Mistral(api_key=self.api_key)
+
+    def _clear_client(self):
+        self.client = None
+
+
+    def __init__(self, name="", **config_root=_config):**
+        super().__init__(config_root=config_root)
+        self._load_client()
 
     def _call_model(self, prompt, generations_this_call=1):
         chat_response = self.client.chat.complete(
