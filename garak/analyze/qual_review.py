859e5ebec4ca7b02562295dff7652242ae9fb05f
Jeffrey Martin <jemartin@nvidia.com>
2025-04-21 17:13:33 -0500

    build tiers from plugin_cache
    * extract tier values from the plugin metadata
    * guard for divide by 0
    
    Signed-off-by: Jeffrey Martin <jemartin@nvidia.com>

--- garak/analyze/qual_review.py
+++ garak/analyze/qual_review.py
@@ -12,145 +12,25 @@ import random
 import sys
 
 import garak.analyze.calibration
+from garak.probes.base import Tier
 
-# hard-code the tiers for now (2025 03 10)
 
-tiers = {
-    "ansiescape.AnsiEscaped": 3,
-    "ansiescape.AnsiRaw": 1,
-    "atkgen.Tox": 2,
-    "av_spam_scanning.EICAR": "u",
-    "av_spam_scanning.GTphish": "u",
-    "av_spam_scanning.GTUBE": "u",
-    "continuation.ContinueSlursReclaimedSlurs": 2,
-    "continuation.ContinueSlursReclaimedSlursMini": 2,
-    "dan.Ablation_Dan_11_0": 2,
-    "dan.AntiDAN": "u",
-    "dan.AutoDAN": 2,
-    "dan.AutoDANCached": 2,
-    "dan.ChatGPT_Developer_Mode_RANTI": "u",
-    "dan.ChatGPT_Developer_Mode_v2": "u",
-    "dan.ChatGPT_Image_Markdown": "u",
-    "dan.Dan_10_0": "u",
-    "dan.Dan_11_0": "u",
-    "dan.Dan_6_0": "u",
-    "dan.Dan_6_2": "u",
-    "dan.Dan_7_0": "u",
-    "dan.Dan_8_0": "u",
-    "dan.Dan_9_0": "u",
-    "dan.DAN_Jailbreak": "u",
-    "dan.DanInTheWild": 1,
-    "dan.DanInTheWildMini": 1,
-    "dan.DUDE": "u",
-    "dan.STAN": "u",
-    "donotanswer.DiscriminationExclusionToxicityHatefulOffensive": "u",
-    "donotanswer.HumanChatbox": "u",
-    "donotanswer.InformationHazard": "u",
-    "donotanswer.MaliciousUses": "u",
-    "donotanswer.MisinformationHarms": "u",
-    "encoding.InjectAscii85": 2,
-    "encoding.InjectBase16": 2,
-    "encoding.InjectBase2048": 2,
-    "encoding.InjectBase32": 2,
-    "encoding.InjectBase64": 1,
-    "encoding.InjectBraille": 2,
-    "encoding.InjectEcoji": 2,
-    "encoding.InjectHex": 2,
-    "encoding.InjectMime": 2,
-    "encoding.InjectMorse": 2,
-    "encoding.InjectNato": 2,
-    "encoding.InjectQP": 1,
-    "encoding.InjectROT13": 2,
-    "encoding.InjectUU": 1,
-    "encoding.InjectZalgo": 2,
-    "fileformats.HF_Files": 1,
-    "glitch.Glitch": 3,
-    "glitch.Glitch100": 3,
-    "goodside.Davidjl": 3,
-    "goodside.Tag": 1,
-    "goodside.ThreatenJSON": 2,
-    "goodside.WhoIsRiley": 2,
-    "grandma.Slurs": 1,
-    "grandma.Substances": 1,
-    "grandma.Win10": 2,
-    "grandma.Win11": 2,
-    "latentinjection.LatentInjectionFactSnippetEiffel": 1,
-    "latentinjection.LatentInjectionReport": 1,
-    "latentinjection.LatentInjectionResume": 1,
-    "latentinjection.LatentInjectionTranslationEnFr": 1,
-    "latentinjection.LatentInjectionTranslationEnZh": 1,
-    "latentinjection.LatentJailbreak": 1,
-    "latentinjection.LatentJailbreakMini": 1,
-    "leakreplay.GuardianCloze": 2,
-    "leakreplay.GuardianComplete": 1,
-    "leakreplay.LiteratureCloze": 2,
-    "leakreplay.LiteratureCloze80": 2,
-    "leakreplay.LiteratureComplete": 1,
-    "leakreplay.LiteratureComplete80": 1,
-    "leakreplay.NYTCloze": 2,
-    "leakreplay.NYTComplete": 1,
-    "leakreplay.PotterCloze": 2,
-    "leakreplay.PotterComplete": 1,
-    "lmrc.Anthropomorphisation": 3,
-    "lmrc.Bullying": 2,
-    "lmrc.Deadnaming": 2,
-    "lmrc.Profanity": 3,
-    "lmrc.QuackMedicine": 2,
-    "lmrc.SexualContent": 2,
-    "lmrc.Sexualisation": 2,
-    "lmrc.SlurUsage": 2,
-    "malwaregen.Evasion": 2,
-    "malwaregen.Payload": 2,
-    "malwaregen.SubFunctions": 2,
-    "malwaregen.TopLevel": 2,
-    "misleading.FalseAssertion50": 2,
-    "packagehallucination.JavaScript": 1,
-    "packagehallucination.PackageHallucinationProbe": "u",
-    "packagehallucination.Python": 1,
-    "packagehallucination.Ruby": 1,
-    "packagehallucination.Rust": 1,
-    "phrasing.FutureTense": 2,
-    "phrasing.FutureTenseMini": 2,
-    "phrasing.PastTense": 2,
-    "phrasing.PastTenseMini": 2,
-    "promptinject.HijackHateHumans": 2,
-    "promptinject.HijackHateHumansMini": 2,
-    "promptinject.HijackKillHumans": 2,
-    "promptinject.HijackKillHumansMini": 2,
-    "promptinject.HijackLongPrompt": 2,
-    "promptinject.HijackLongPromptMini": 2,
-    "realtoxicityprompts.RTPBlank": 1,
-    "realtoxicityprompts.RTPFlirtation": 3,
-    "realtoxicityprompts.RTPIdentity_Attack": 3,
-    "realtoxicityprompts.RTPInsult": 3,
-    "realtoxicityprompts.RTPProfanity": 3,
-    "realtoxicityprompts.RTPSevere_Toxicity": 3,
-    "realtoxicityprompts.RTPSexually_Explicit": 3,
-    "realtoxicityprompts.RTPThreat": 3,
-    "divergence.Repeat": 2,
-    "divergence.RepeatExtended": 2,
-    "snowball.GraphConnectivity": 2,
-    "snowball.GraphConnectivityMini": 2,
-    "snowball.Primes": 3,
-    "snowball.PrimesMini": 3,
-    "snowball.Senators": 3,
-    "snowball.SenatorsMini": 3,
-    "suffix.BEAST": 2,
-    "suffix.GCG": 2,
-    "suffix.GCGCached": 2,
-    "tap.PAIR": 2,
-    "tap.TAP": 2,
-    "tap.TAPCached": 2,
-    "test.Blank": "u",
-    "test.Test": "u",
-    "topic.WordnetAllowedWords": "u",
-    "topic.WordnetBlockedWords": "u",
-    "topic.WordnetControversial": 2,
-    "visual_jailbreak.FigStep": 2,
-    "visual_jailbreak.FigStepTiny": 2,
-    "xss.MarkdownImageExfil": 1,
-}
+def build_tiers() -> dict:
+    from garak._plugins import enumerate_plugins, plugin_info
 
+    tiers = {}
+    for tier in Tier:
+        tiers[tier] = []
+
+    for plugin, _ in enumerate_plugins("probes"):
+        details = plugin_info(plugin)
+        _, plugin_name = plugin.split(".", 1)
+        tiers[Tier(details["tier"])].append(plugin_name)
+
+    return tiers
+
+
+tiers = build_tiers()
 # load in the scores
 
 c = garak.analyze.calibration.Calibration()
@@ -165,7 +45,7 @@ with open(sys.argv[1], "r", encoding="utf-8") as report_file:
     g = (json.loads(line.strip()) for line in open(sys.argv[1]) if line.strip())
     for record in g:
         if record["entry_type"] == "eval":
-            passrate = record["passed"] / record["total"]
+            passrate = record["passed"] / record["total"] if record["total"] > 0 else 0
             probe_module, probe_classname = record["probe"].split(".", 1)
             detector = record["detector"].replace("detector.", "")
             detector_module, detector_classname = detector.split(".", 1)
@@ -220,7 +100,7 @@ def _print_examples(probe_detector):
 
 
 print("Tier 1")
-t1_probe_names = [probe_name for probe_name, tier in tiers.items() if tier == 1]
+t1_probe_names = tiers[Tier.TIER_1]
 for probe_detector in probe_detector_scores.keys():
     if probe_detector.split("_")[0] in t1_probe_names:
         z, passrate = (
@@ -242,7 +122,7 @@ for probe_detector in probe_detector_scores.keys():
             )
 
 print("\nTier 2")
-t2_probe_names = [probe_name for probe_name, tier in tiers.items() if tier == 2]
+t2_probe_names = tiers[Tier.TIER_2]
 for probe_detector in probe_detector_scores.keys():
     if probe_detector.split("_")[0] in t2_probe_names:
         z, passrate = (
@@ -260,5 +140,9 @@ for probe_detector in probe_detector_scores.keys():
 
 print("\nNot processed:")
 processed_probes = t1_probe_names + t2_probe_names
-for entry in [probe_detector for probe_detector in probe_detector_scores.keys() if probe_detector.split("_")[0] not in processed_probes]:
+for entry in [
+    probe_detector
+    for probe_detector in probe_detector_scores.keys()
+    if probe_detector.split("_")[0] not in processed_probes
+]:
     print(entry)
