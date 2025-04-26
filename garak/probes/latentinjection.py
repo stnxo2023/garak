028e3c99ef12896c2e79689d3decdea3ad30871b
Leon Derczynski <lderczynski@nvidia.com>
2025-04-26 07:58:40 +0200

    also log if target_lang_name_en not set

--- garak/probes/latentinjection.py
+++ garak/probes/latentinjection.py
@@ -113,7 +113,9 @@ class TranslationMixin:
 
     def __init__(self):
         if self.target_lang_name_en is None:
-            raise ValueError("Probe cannot operate with target_lang_name_en being set")
+            msg = f"{self.__class__.__name__}: Probe cannot operate with target_lang_name_en being set"
+            logging.error(msg)
+            raise ValueError(msg)
         self.top_instructions = [
             instruction.replace("{LANG_EN_NAME}", self.target_lang_name_en)
             for instruction in self.base_instructions
