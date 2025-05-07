60775f6ab4024eb8f653b9573bcc53d6db1dc385
Leon Derczynski <leonderczynski@gmail.com>
2025-05-07 12:48:31 +0200

    test for reqs presence in pyproject.toml, requirements.txt

--- tests/test_reqs.py
+++ tests/test_reqs.py
@@ -1,12 +1,16 @@
 # SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+import importlib
 import pytest
+import re
 
 try:
     import tomllib
 except:
     tomllib = None
+
+import garak._plugins
 
 
 @pytest.mark.skipif(
@@ -40,3 +44,75 @@ def test_requirements_txt_pyproject_toml():
     assert (
         reqtxt_reqs == pyproject_reqs
     )  # final check. this one is actually enough, but let's help us debug by finding which test fails, ok?
+
+
+PLUGIN_TYPES = ["probes", "detectors", "harnesses", "generators", "buffs"]
+
+
+def plugin_names():
+    plugin_names = set()
+    for plugin_type in PLUGIN_TYPES:
+        plugin_names.update(
+            [n for (n, active) in garak._plugins.enumerate_plugins(plugin_type)]
+        )
+    return plugin_names
+
+
+def split_out_module_name(pep508_descr: str):
+    return re.split(r"[\<\>\=\!]", pep508_descr)[0]
+
+
+def requirement_names():
+    with open("requirements.txt", "r", encoding="utf-8") as req_file:
+        reqtxt_reqs = req_file.readlines()
+        reqtxt_reqs = list(
+            filter(lambda x: not x.startswith("#"), map(str.strip, reqtxt_reqs))
+        )
+        requirement_names = set([split_out_module_name(r) for r in reqtxt_reqs])
+    return requirement_names
+
+
+def pyproject_optional_dep_names():
+    with open("pyproject.toml", "rb") as pyproject_file:
+        pyproject_toml = tomllib.load(pyproject_file)
+    optional_deps = set()
+    for test_group in pyproject_toml["project"]["optional-dependencies"]:
+        if test_group.startswith("plugin_"):
+            group_dep_names = [
+                split_out_module_name(r)
+                for r in pyproject_toml["project"]["optional-dependencies"][test_group]
+            ]
+            optional_deps.update(group_dep_names)
+    return optional_deps
+
+
+@pytest.mark.parametrize("plugin_name", plugin_names())
+def test_optional_extras_not_in_requirements(plugin_name: str):
+    m = importlib.import_module("garak." + ".".join(plugin_name.split(".")[:-1]))
+    plugin_class = getattr(m, plugin_name.split(".")[-1])
+    plugin_extra_dep_names = [
+        d.split(".")[0] for d in plugin_class.extra_dependency_names
+    ]
+    extra_deps_in_requirements = requirement_names().intersection(
+        plugin_extra_dep_names
+    )
+    assert len(extra_deps_in_requirements) == 0, (
+        "extra deps should not be in requirements.txt but %s overlaps"
+        % extra_deps_in_requirements
+    )
+
+
+@pytest.mark.parametrize("plugin_name", plugin_names())
+def test_optional_extras_not_in_pyproject(plugin_name: str):
+    m = importlib.import_module("garak." + ".".join(plugin_name.split(".")[:-1]))
+    plugin_class = getattr(m, plugin_name.split(".")[-1])
+    plugin_extra_dep_names = [
+        d.split(".")[0] for d in plugin_class.extra_dependency_names
+    ]
+    extra_deps_in_pyproject = pyproject_optional_dep_names().intersection(
+        plugin_extra_dep_names
+    )
+    assert len(extra_deps_in_pyproject) == len(plugin_extra_dep_names), (
+        "all extra dependences %s must be in optional plugin clause in pyproject.toml"
+        % plugin_extra_dep_names
+    )
