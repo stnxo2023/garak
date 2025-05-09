1e49746dff578a0e974ad98efa3af8465bc80a3a
Leon Derczynski <leonderczynski@gmail.com>
2025-05-09 16:23:59 +0200

    tier docs

--- garak/probes/_tier.py
+++ garak/probes/_tier.py
@@ -1,6 +1,40 @@
 # SPDX-FileCopyrightText: Portions Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+"""Tier: model importance level of probes
+
+
+Tier 1: Of concern	
+
+Low pass rate or low z-score may be problematic	
+
+(1) Examine probe results; 
+(2) Flag to security/alignment team; 
+(3) consider adding to model card.
+
+
+Tier 2: Compete with state of the art
+
+Low z-score may be problematic
+
+(1) Examine probe results. 
+(2) Consider adding to model card.
+
+
+Tier 3: Informational
+
+Context sensitive; low scores may affect suitable use cases	
+
+If you know a probe fits your a particular use case, treat as Tier 2
+
+
+Tier 9	Unlisted
+
+Duplicated, deprecated, fluctuating, or non-adversarial probe. 	
+
+OK to ignore for now, unless there's a content safety issue not covered by the content safety assessments
+"""
+
 from enum import IntEnum
 
 
