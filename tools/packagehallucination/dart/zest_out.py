96c258e3012072a45c98ce38d94ce7eb7c07b721
dchiitmalla <dchitimalla@gmail.com>
2025-05-30 14:19:07 -0700

    remove zest file

--- tools/packagehallucination/dart/zest_out.py
+++
@@ -1,6 +0,0 @@
-from datasets import load_dataset
-
-ds = load_dataset("dchitimalla1/perl-20250529", split="train")
-print(ds.column_names)        # ['name']
-print(ds[:1000])                 # Preview top 5
-print("http" in ds["name"])   # True if present
