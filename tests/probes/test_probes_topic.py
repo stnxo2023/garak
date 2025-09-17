8ba8fc799d683b58b222be084fceb346576cb2ce
dhruvmalik007 <malikdhruv1994@gmail.com>
2025-09-17 16:44:20 +0000

    refactoring function names and return types , as suggesgted by @jmartin-tech :

--- tests/probes/test_probes_topic.py
+++ tests/probes/test_probes_topic.py
@@ -14,12 +14,12 @@ TEST_TERM = "abortion"
 TEST_SYNSET_ID = "oewn-00231191-n"
 
 @pytest.fixture(scope="module")
-def setup_wordnetEnv():
+def sysnet():
     garak.probes.topic.WordnetBlockedWords()
     wn.download(TEST_LEXICON)
     w = wn.Wordnet(TEST_LEXICON)
     s = w.synset(TEST_SYNSET_ID)
-    return (w, s)
+    return s
 
 
 PROBES = [
@@ -42,35 +42,31 @@ def test_topic_wordnet_version(probename):
 
 
 @pytest.mark.parametrize("probename", PROBES)
-def test_topic_wordnet_get_node_terms(probename, setup_wordnetEnv):
+def test_topic_wordnet_get_node_terms(probename, sysnet):
     p = garak._plugins.load_plugin(probename)
-    _, s = setup_wordnetEnv
-    terms = p._get_node_terms(s)
+    terms = p._get_node_terms(sysnet)
     assert list(terms) == ["abortion"]
 
 
 @pytest.mark.parametrize("probename", PROBES)
-def test_topic_wordnet_get_node_children(probename, setup_wordnetEnv):
+def test_topic_wordnet_get_node_children(probename, sysnet):
     p = garak._plugins.load_plugin(probename)
-    _, s = setup_wordnetEnv
-    children = p._get_node_children(s)
+    children = p._get_node_children(sysnet)
     assert children == [wn.synset("oewn-00231342-n"), wn.synset("oewn-00232028-n")]
 
 
 @pytest.mark.parametrize("probename", PROBES)
-def test_topic_wordnet_get_node_id(probename, setup_wordnetEnv):
+def test_topic_wordnet_get_node_id(probename, sysnet):
     p = garak._plugins.load_plugin(probename)
-    _, s = setup_wordnetEnv
-    assert p._get_node_id(s) == TEST_SYNSET_ID
+    assert p._get_node_id(sysnet) == TEST_SYNSET_ID
 
 
-def test_topic_wordnet_blocklist_get_initial_nodes(setup_wordnetEnv):
-    _, s = setup_wordnetEnv
+def test_topic_wordnet_blocklist_get_initial_nodes(sysnet):
     p = garak.probes.topic.WordnetBlockedWords()
     p.target_topic = TEST_TERM
     initial_nodes = p._get_initial_nodes()
     assert initial_nodes == [
-        s,
+        sysnet,
         wn.synset("oewn-07334252-n"),
         wn.synset("oewn-00231342-n"),
         wn.synset("oewn-00232028-n"),
