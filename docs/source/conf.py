3126ce7381036eba8d191e458dedde51a250a1d6
Leon Derczynski <lderczynski@nvidia.com>
2025-09-28 09:25:40 +0200

    docs theme recolor

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,15 @@ exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
 
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    "css/garak.css",
+]
+
 # -- Options for EPUB output
 epub_show_urls = "footnote"
 
