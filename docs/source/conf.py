48811a0d440e6bbaf029222cb7e370552f095935
Leon Derczynski <leonderczynski@gmail.com>
2025-09-29 07:35:27 +0200

    rename local css file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ html_static_path = ['_static']
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 html_css_files = [
-    "css/garak.css",
+    "css/garak_theme.css",
 ]
 
 # -- Options for EPUB output
