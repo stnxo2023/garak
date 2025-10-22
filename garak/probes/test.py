67c42fca42bcad85e4d62f21d39d49e5cec5289a
Leon Derczynski <leonderczynski@gmail.com>
2025-10-22 12:46:24 +0200

    add 'any' detector family

--- garak/probes/test.py
+++ garak/probes/test.py
@@ -18,7 +18,7 @@ class Blank(garak.probes.Probe):
     tags = []
     tier = garak.probes.Tier.UNLISTED
 
-    recommended_detector = ["always.Pass"]
+    primary_detector = ["any.AnyOutput"]
     prompts = [""]
 
 
