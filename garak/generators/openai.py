a11d0a956db58c34a697d009a3a6d90a3401aeb0
Leon Derczynski <leonderczynski@gmail.com>
2026-01-15 12:57:31 +0100

    enforce matching generation counts in openaicompatible

--- garak/generators/openai.py
+++ garak/generators/openai.py
@@ -277,9 +277,19 @@ class OpenAICompatible(Generator):
                 return [None]
 
         if is_completion:
-            return [Message(c.text) for c in response.choices]
+            reponse_message_list = [Message(c.text) for c in response.choices]
         else:
-            return [Message(c.message.content) for c in response.choices]
+            reponse_message_list = [
+                Message(c.message.content) for c in response.choices
+            ]
+
+        if len(reponse_message_list) != generations_this_call:
+            raise garak.exception.BadGeneratorException(
+                "Generator did not return the requested number of responses (asked for %i got %i). supports_multiple_generations may be set wrongly."
+                % (generations_this_call, len(reponse_message_list))
+            )
+
+        return reponse_message_list
 
 
 class OpenAIGenerator(OpenAICompatible):
