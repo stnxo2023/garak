64bb7739bea4d89bd2e15db760217c8ecd8c9fba
Leon Derczynski <leonderczynski@gmail.com>
2026-01-29 13:10:50 +0100

    factor up defcon assignment, streamline digest construction

--- garak/analyze/tbsa.py
+++ garak/analyze/tbsa.py
@@ -24,19 +24,6 @@ import garak.resources.theme
 PROBE_DETECTOR_SEP = "+"
 
 
-def map_score(score: float) -> int:
-    """assign a defcon class (i.e. 1-5, 1=worst) to a %age score 0.0-100.0"""
-    if score < garak.analyze.ABSOLUTE_DEFCON_BOUNDS.TERRIBLE * 100.0:
-        return 1
-    if score < garak.analyze.ABSOLUTE_DEFCON_BOUNDS.BELOW_AVG * 100.0:
-        return 2
-    if score < garak.analyze.ABSOLUTE_DEFCON_BOUNDS.ABOVE_AVG * 100.0:
-        return 3
-    if score < garak.analyze.ABSOLUTE_DEFCON_BOUNDS.EXCELLENT * 100.0:
-        return 4
-    return 5
-
-
 def build_tiers() -> dict:
     from garak._plugins import enumerate_plugins, plugin_info
 
@@ -115,14 +102,6 @@ def digest_to_tbsa(digest: dict, verbose=False) -> Tuple[float, str]:
 
     pd_aggregate_defcons = {}
     for probe_detector, scores in probe_detector_defcons.items():
-        # if scores["relative"] is not None and scores["relative"] != "n/a":
-        # relative_defcon, _ = c.defcon_and_comment(scores["relative"])
-        # else:
-        # relative_defcon = None
-        # absolute_defcon = map_score(scores["absolute"])
-
-        # if verbose:
-        #    print("process>", probe_detector, scores)
 
         if probe_detector in tiers[1]:
             if isinstance(scores["relative"], float):
