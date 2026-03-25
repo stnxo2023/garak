3101d5c46e95d1f0c40dca83b62015dd25f782c6
Patricia Pampanelli <ppampanelli@nvidia.com>
2026-03-25 06:14:22 -0700

    Fix SPDX headers: consistent style and update copyright year to 2026
    Signed-off-by: Patricia Pampanelli <ppampanelli@nvidia.com>

--- tests/detectors/test_detectors_refusal.py
+++ tests/detectors/test_detectors_refusal.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Portions Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 import pytest
